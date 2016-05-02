f6cd807d49028c25fac4ad2c377c23cefb0465c7
Wanpat Anantapan <oneplus1000@gmail.com>
2016-05-02 09:39:22 +0700

    change Curr to unexport value

--- calwidth/main.go
+++ calwidth/main.go
@@ -26,19 +26,11 @@ func main() {
 	//pdf.SetGrayFill(0.5)
 	//pdf.Cell(nil, "Áa")
 
+	//Measure Width
 	text := "How can I cordinate the text that I want draw?"
 	pdf.Cell(nil, text)
-	sfont := pdf.Curr.Font_ISubset
-	sumWidth := uint64(0)
-	for _, r := range text {
-		width, err := sfont.CharWidth(r)
-		if err != nil {
-			log.Print(err.Error())
-			return
-		}
-		sumWidth = sumWidth + width
-	}
-	realWidth := float64(sumWidth) * (float64(fontSize) / 1000.0) //convert
+
+	realWidth, _ := pdf.MeasureTextWidth(text)
 	fmt.Printf("realWidth = %f", realWidth)
 
 	var parser core.TTFParser
@@ -47,6 +39,8 @@ func main() {
 		log.Print(err.Error())
 		return
 	}
+
+	//Measure Height
 	//get  CapHeight (https://en.wikipedia.org/wiki/Cap_height)
 	cap := float64(float64(parser.CapHeight()) * 1000.00 / float64(parser.UnitsPerEm()))
 	//convert
